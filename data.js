c6b450d8cfb4ed51cfc207beda33b31d77698a74
antoniomrc1981 <103452220+antoniomrc1981@users.noreply.github.com>
2023-09-19 10:39:35 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,2 +1,3 @@
-const iframeLink = "INSERISCI QUELLO CHE FINISCE CON__________/viewform?embedded=true";
+const iframeLink = "https://forms.gle/d87S5C35sYuHLDm57";
+//"INSERISCI QUELLO CHE FINISCE CON__________/viewform?embedded=true";//
 const unlockPin = "4396";

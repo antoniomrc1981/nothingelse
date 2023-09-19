da0ea60fda56459d6e0fc6b07f8b52504f0513bf
antoniomrc1981 <103452220+antoniomrc1981@users.noreply.github.com>
2023-09-19 10:40:47 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-const iframeLink = "https://forms.gle/d87S5C35sYuHLDm57";
+const iframeLink = "<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeNjowO7EAYN2IdXZVIlSEq4a4gcNet9jRB15Itb36tuhHXTg/viewform?embedded=true" width="640" height="15003" frameborder="0" marginheight="0" marginwidth="0">Caricamento…</iframe>";
 //"INSERISCI QUELLO CHE FINISCE CON__________/viewform?embedded=true";//
 const unlockPin = "4396";
